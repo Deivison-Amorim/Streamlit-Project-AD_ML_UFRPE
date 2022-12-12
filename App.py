df180f9cf5f94518ac14293eb93b323497410e19
Deivison-Amorim <deivison.gomes.amorim@gmail.com>
2022-12-12 17:04:48 -0300

    sexto commit

--- App.py
+++ App.py
@@ -5,15 +5,7 @@ import altair as alt
 import time as time
 
 dataset = pd.read_csv(
-    'Streamlit_AD_ML_UFRPE/Arquivos_Alunos/dataset_review_books_csv')
-
-# dataset = pd.read_excel(
-#    io='F:\Streamlit\Arquivos_Alunos\Datasets\dataset_review_books.xlsx',
-#    engine='openpyxl',
-#    sheet_name='Plan1',
-#    usecols='A:B',
-#    nrows=2001
-# )
+    'F:\Streamlit\dataset_review_books_csv.csv')
 
 with st.sidebar:
     st.subheader("DASHBOARD - MENU")
